e630e750534379abcabdc9e99a09700fae1d5b79
RustamOper05 <shabutdinov200@gmail.com>
2023-10-05 11:51:02 +0300

    done lab-4

--- js/script.js
+++ js/script.js
@@ -5,5 +5,19 @@
         var endTime = new Date().getTime();
         var loadTime = endTime - startTime;
         document.getElementById('loadTime').innerHTML = 'Page loaded in ' + loadTime + 'ms';
+
+        var currentUrl = document.location;
+        // Получаем все пункты меню
+        var menuItems = document.querySelectorAll('.nav_li');
+        // Перебираем все пункты меню
+        for (var i = 0; i < menuItems.length; i++) {
+            // Получаем ссылку пункта меню
+            var link = menuItems[i].querySelector('a');
+            // Проверяем, содержит ли ссылка текущий URL страницы
+            if (currentUrl["href"].includes(link.href)) {
+                // Добавляем класс для активного состояния
+                menuItems[i].classList.add('nav_active');
+            }
+        }
     });
 })();
